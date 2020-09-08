7c027a820a7ebcc41792830b9505de7ea1b0e3b0
watacle <sirwatacle@gmail.com>
2020-09-08 08:47:07 +0200

    added path, shocking

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const path = require('path')
 const app = express();
 
 app.use(express.json())
